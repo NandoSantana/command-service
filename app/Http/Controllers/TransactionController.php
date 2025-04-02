a38431f04114f86875e5f447b40e418819322f4b
Fernando Santana <bh.carvalho@gmail.com>
2025-04-02 17:23:11 -0300

    trasnaction controller

---
+++ app/Http/Controllers/TransactionController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Services\TransactionService;
+use Illuminate\Http\JsonResponse;
+
+
+class TransactionController extends Controller
+{
+
+    protected $transactionService;
+
+    public function __construct(TransactionService $transactionService)
+    {
+        $this->transactionService = $transactionService;
+    }
+
+    public function deposit(Request $request): JsonResponse
+    {
+        $data = $request->validate([
+            'user_id' => 'required|exists:users,id',
+            'amount' => 'required|numeric|min:0.01',
+        ]);
+
+        return response()->json($this->transactionService->deposit($data['user_id'], $data['amount']));
+    }
+
+    public function transfer(Request $request): JsonResponse
+    {
+        $data = $request->validate([
+            'payer_id' => 'required|exists:users,id',
+            'payee_id' => 'required|exists:users,id|different:payer_id',
+            'amount' => 'required|numeric|min:0.01',
+        ]);
+
+        return response()->json($this->transactionService->transfer($data['payer_id'], $data['payee_id'], $data['amount']));
+    }
+
+}
