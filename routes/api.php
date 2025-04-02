caa3f5270e7f82878018c8dd14d94d21b3fc0c18
Fernando Santana <bh.carvalho@gmail.com>
2025-04-02 18:04:53 -0300

    fix route for api

--- routes/api.php
+++ routes/api.php
@@ -1,4 +1,7 @@
+<?php 
+
 use App\Http\Controllers\TransactionController;
+use Illuminate\Support\Facades\Route;
 
 Route::post('/deposit', [TransactionController::class, 'deposit']);
 Route::post('/transfer', [TransactionController::class, 'transfer']);
